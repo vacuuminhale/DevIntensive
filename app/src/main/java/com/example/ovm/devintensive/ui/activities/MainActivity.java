f62edb61411781a15ae45e7b0b3e6e0ee149dea4
vacuuminhale <ovm1834@gmail.com>
2016-07-03 22:18:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ovm/devintensive/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ovm.devintensive.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ovm.devintensive.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
